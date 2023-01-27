68a3997eec05d7e6a387a51627c0fcd55c8f01fa
Joseph Unomieta <lionelunomieta@gmail.com>
2023-01-27 11:50:13 +0100

    simple counter app with bloc state management

---
+++ lib/app.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_counter/counter/counter.dart';
+import 'package:flutter_counter/counter/view/counter_page.dart';
+
+class CounterApp extends MaterialApp{
+   CounterApp({super.key}) : super(home: CounterPage());
+}
